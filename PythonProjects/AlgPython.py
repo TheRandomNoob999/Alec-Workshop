4d3fa371ae3d167dbffb4d51cd81c42a1ae81125
TheRandomNoob999 <119257101+TheRandomNoob999@users.noreply.github.com>
2024-07-12 23:33:30 -0400

    Working On This

---
+++ PythonProjects/AlgPython.py
@@ -0,0 +1,12 @@
+def DisplayChoices():
+
+    print("Selection Sort: 1")
+
+def MainMenu():
+
+    UserInput = input("\nChoose Algorithm: ")
+
+    if UserInput == "1":
+        print("Selection Sort Selected!")
+    else:
+        print("Error please select a number corresponding to the Algorithm you want.")
